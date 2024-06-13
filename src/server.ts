d37630233818e72b25f3345d5dade777d72159f9
TijanAyo <ayomidetijani6@gmail.com>
2024-06-13 21:26:10 +0100

    Chore: Added global appResponse for consistent response and readability

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,62 @@ dotenv.config();
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { allowedOrigins, connectDB, isLocal } from "./config";
+import { AppResponse } from "./utils";
+import Http from "./constants/statusCodes";
 
 const app: Express = express();
 const PORT = Number(process.env.PORT) || 5000;
 
+// Middleware
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (_req: Request, res: Response) => {
+  return AppResponse(res, Http.OK, null, "Server up and running 🚀🚀", true);
+});
+
+app.get("/health", (_req: Request, res: Response) => {
+  return AppResponse(
+    res,
+    Http.OK,
+    null,
+    "Server is healthy and running smoothly 🏃🏾🏃🏾",
+    true,
+  );
+});
+
+// 404 Route
+app.all("*", (_req: Request, res: Response) => {
+  return AppResponse(
+    res,
+    Http.NOT_FOUND,
+    null,
+    "Route does not exist, check provided endpoint and try again",
+    false,
+  );
+});
+
+const startServer = async () => {
+  try {
+    // DB Config
+    await connectDB();
+    app.listen(PORT, () => {
+      isLocal
+        ? console.info(`Server running on http://localhost:${PORT}`)
+        : console.info(`Server running on prod`);
+    });
+  } catch (err: any) {
+    console.error(`Failed to connect to the database: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
